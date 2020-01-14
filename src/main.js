c4d59711ae70dc13de40106f2da0dadf4718d136
kuhandisangmo <1252039116@qq.com>
2020-01-14 15:38:34 +0800

    配置完成,开始项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入通用样式
+import './styles/base.css'
+import './styles/iconfont.css'
+import 'lib-flexible'
+import router from './router/index'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
